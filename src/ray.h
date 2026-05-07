5102a02cff84280d849a713b8a0cf8e0f6a1954d
EKmaster <omerekwiz@gmail.com>
2026-05-06 20:11:42 -0400

    Added BVH, AABBs, and Perlin noise

--- src/ray.h
+++ src/ray.h
@@ -4,7 +4,8 @@
 class ray {
   public:
     ray() {}
-    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+    ray(const point3& origin, const vec3& direction, double time)
+        : orig(origin), dir(direction), tm(time) {}
     const point3& origin() const { return orig; }
     const vec3& direction() const { return dir; }
     point3 at(double t) const { return orig + t * dir; }
